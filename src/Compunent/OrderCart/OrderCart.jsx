e8a3bbd3b003c69dc513ef334258cd1620a9276f
Mostafa Mohsin <mostafamohsin783@gmail.com>
2023-12-30 00:45:45 +0600

    dataAdd

--- src/Compunent/OrderCart/OrderCart.jsx
+++ src/Compunent/OrderCart/OrderCart.jsx
@@ -1,10 +1,74 @@
+import { useEffect, useState } from "react";
 
 const OrderCart = () => {
-    return (
-        <div>
-            
+    const [data,setData] = useState([]);
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('http://localhost:5000/getPostData');
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+  
+          const result = await response.json();
+          setData(result);
+        } catch (error) {
+          console.error('Error fetching data:', error.message);
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
+
+
+  return (
+    <body className="bg-gray-100">
+      <div className="container mx-auto mt-10">
+        <div className="shadow-md my-10">
+          <div className="lg:flex justify-center">
+            <div className="w-full lg:w-3/4 bg-white px-6 lg:px-10 py-10">
+              <div className="flex justify-between border-b pb-8">
+                <h1 className="font-semibold text-2xl">Shopping Cart</h1>
+                <h2 className="font-semibold text-2xl">{data.length} Items</h2>
+              </div>
+              <div className="max-sm:hidden flex flex-col mt-10 mb-5 md:flex-row">
+                <h3 className=" font-semibold text-gray-600 text-xs uppercase w-full md:w-2/5">Product Details</h3>
+                <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Quantity</h3>
+                <h3 className="font-semibold text-center text-gray-600 text-xs uppercase  w-1/5 text-center">Price</h3>
+                <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
+              </div>
+              {data.map((item) => (
+                <div key={item.id} className="flex bg-green-100 items-center hover:bg-gray-100 -mx-2 md:-mx-8 px-4 md:px-6 py-4 md:py-5">
+                  <div className="flex w-full md:w-2/5">
+                    <div className="w-20">
+                      <img className="h-24 w-24" src={item.img} alt={item.name} />
+                    </div>
+                    <div className="flex flex-col justify-between ml-4 md:ml-0 md:flex-grow">
+                      <span className="font-bold text-sm">{item.name}</span>
+                      <span className="text-red-500 text-xs">{item.brand}</span>
+                      <a href="#" className="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
+                    </div>
+                  </div>
+                  <div className="flex justify-center w-1/5">
+                    <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
+                      {/* ... */}
+                    </svg>
+                 <p>{item.quantity}</p>
+                    <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
+                      {/* ... */}
+                    </svg>
+                  </div>
+                  <span className="max-sm:hidden text-center w-1/5 font-semibold text-sm">${item.price.toFixed(2)}</span>
+                  <span className="text-center w-1/5 font-semibold text-sm">${(item.price * item.quantity).toFixed(2)}</span>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </body>
+  );
 };
 
 export default OrderCart;
